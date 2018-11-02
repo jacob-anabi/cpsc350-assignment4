4323f1fca44de9595aefeb93b69b23b3b2e192b8
Jacob Anabi <anabi@chapman.edu>
2018-11-01 22:33:10 -0700

    deleted simulation aftering running it

--- main.cpp
+++ main.cpp
@@ -19,6 +19,7 @@ int main(int argc, char** argv)
     {
         Simulation* simulation = new Simulation(argv[1]);
         simulation->run();
+        delete simulation;
     }
     catch (RuntimeException& err)
     {
